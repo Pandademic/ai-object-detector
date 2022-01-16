3cd91e46e97e3d51871778ab49a19eb2d58154f5
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-16 17:28:05 -0500

    add JS

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup(){
+    canvas = createaCanvas(640,420);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
+}
+function draw(){
+   image(video,0,0,640,420);//cover the canvas with camera
+}
+function modelLoaded(){
+    document.getElementById('status').innerHTML = "Model Loaded !";
+    setTimeout(() => { document.getElementById('status').innerHTML = "Status : Detecting Objects"; }, 7000);
+  
+
+}
