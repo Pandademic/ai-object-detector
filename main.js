7bdfe5e34b5e1ba751c927a1ec90a2f52d5c72d1
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-03-10 18:11:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
     video = createCapture(VIDEO);
+    video.hide()
 }
 function draw(){
-   video.parent("camera_div")
    objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
     
 }
 function modelLoaded(){
-    document.getElementById('status').innerHTML = "Model Loaded !";
+    console.log("model loaded!")
+    document.getElementById('status').innerHTML = "Status : Model Loaded";
     setTimeout(() => { document.getElementById('status').innerHTML = "Status : Detecting Objects"; }, 7000);
 }
